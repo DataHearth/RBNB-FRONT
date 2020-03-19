306ef7c3cd4c517169c4f785c9f644c3dab222d4
mw3xp <mw3xp@DESKTOP-OTHIE19>
2020-03-19 13:52:57 +0100

    home page ready

--- rbnb-front/src/app-script.js
+++ rbnb-front/src/app-script.js
@@ -3,15 +3,25 @@ import $ from "jquery";
 $(document).ready(function(){
     
     $(".collapse-menu").click(()=>{
-        if($(".items")[0].style.display == "inline-grid")
+        if($(".items")[0].style.display == "inline-grid"){
             $(".items").css("display", "none");
-        else
+            $(".home").css("transform", "translateY(0)");
+        }
+        else{
             $(".items").css("display", "inline-grid");
+
+            let itemsHeight = $(".items")[0].offsetHeight;
+            $(".home").css("transform", "translateY(" + itemsHeight +"px)");
+        }
     });
 
     $(window).resize(()=>{
-        if($(window).width() >= 900)
+        console.log($(window).width());
+        if($(window).width() >= 900 - 15){
             $(".items").css("display", "none");
+            $(".home").css("transform", "translateY(0)");
+        }
+
         
     })
 
