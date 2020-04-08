21d6e3ba3d4c9e0e6395ee34def56fce4a66cde8
DataHearth <antoine.l@antoine-langlois.net>
2020-04-08 10:25:07 +0200

    add basic authentication

--- src/lib/authenticate.js
+++ src/lib/authenticate.js
@@ -1,31 +1,80 @@
 import firebase from '../firebase';
+import logger from './logger';
 
-export default async function authenticate(email, password) {
+async function setCurrentToken() {
   try {
-    await firebase.auth().signInWithEmailAndPassword(email, password);
     const token = await firebase.auth().currentUser.getIdToken();
 
     localStorage.setItem('authToken', token);
+    return;
   } catch (error) {
-    switch (error.code) {
-      case 'auth/user-not-found':
-        // User not authenticate
-        break;
-      case 'auth/user-disabled':
-        // User disabled
-        break;
-      case 'auth/invalid-email':
-        // Do something when wrong email
-        break;
-      case 'auth/wrong-password':
-        // Do something when wrong email
-        break;
-      default:
-        break;
-    }
+    logger.error('Error while setting up local storage token');
+
+    throw error;
   }
 }
 
-export default async function register(email, password, ) {
-  
+export async function authenticate(email, password) {
+  try {
+    await firebase.auth().signInWithEmailAndPassword(email, password);
+
+    Promise.all(setCurrentToken());
+  } catch (error) {
+    logger.error(error);
+
+    const formattedError = new Error();
+    formattedError.name = 'Register error';
+
+    switch (error.code) {
+      case 'auth/user-disabled':
+        formattedError.message = 'Votre compte a été désactivé';
+        break;
+      case 'auth/user-not-found':
+        formattedError.message = 'Utilisateur non trouvé (email invalide)';
+        break;
+      case 'auth/wrong-password':
+        formattedError.message = 'Mauvais mot de passe';
+        break;
+      case 'auth/invalid-email':
+        formattedError.message = 'Veuillez entrer un email valide (email@example.com)';
+        break;
+      default:
+        formattedError.message = 'Erreur interne...\nVeuillez réessayer plus tard';
+        break;
+    }
+
+    throw formattedError;
+  }
+}
+
+export async function register(data) {
+  const { email, password } = data;
+
+  try {
+    await firebase.auth().createUserWithEmailAndPassword(email, password);
+
+    Promise.all(setCurrentToken());
+  } catch (error) {
+    logger.error(error);
+
+    const formattedError = new Error();
+    formattedError.name = 'Register error';
+
+    switch (error.code) {
+      case 'auth/email-already-in-use':
+        formattedError.message = 'L\'email est déjà utilisé';
+        break;
+      case 'auth/invalid-email':
+        formattedError.message = 'Veuillez entrer un email valide (email@example.com)';
+        break;
+      case 'auth/weak-password':
+        formattedError.message = 'Mot de passe trop faible';
+        break;
+      default:
+        formattedError.message = 'Erreur interne...\nVeuillez réessayer plus tard';
+        break;
+    }
+
+    throw formattedError;
+  }
 }
