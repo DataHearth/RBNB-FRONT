b0346c5dc062becd3bc523771c8eab695e690f30
DataHearth <antoine.l@antoine-langlois.net>
2020-04-08 10:55:54 +0200

    request status handling

--- src/lib/authenticate.js
+++ src/lib/authenticate.js
@@ -1,14 +1,33 @@
+import axios from 'axios';
 import firebase from '../firebase';
 import logger from './logger';
+
+export async function getAuthToken() {
+  const sessionToken = sessionStorage.getItem('authToken');
+
+  if (sessionStorage.getItem('authToken') === null) {
+    try {
+      const firebaseAuthToken = await firebase.auth().currentUser.getIdToken();
+
+      return firebaseAuthToken;
+    } catch (error) {
+      logger.error(error);
+
+      return null;
+    }
+  }
+
+  return sessionToken;
+}
 
 async function setCurrentToken() {
   try {
     const token = await firebase.auth().currentUser.getIdToken();
 
-    localStorage.setItem('authToken', token);
+    sessionStorage.setItem('authToken', token);
     return;
   } catch (error) {
-    logger.error('Error while setting up local storage token');
+    logger.error('Error while setting up session storage auth-token');
 
     throw error;
   }
@@ -53,7 +72,31 @@ export async function register(data) {
   try {
     await firebase.auth().createUserWithEmailAndPassword(email, password);
 
-    Promise.all(setCurrentToken());
+    await setCurrentToken();
+
+    const response = await axios.put('localhost:8080/users', { ...data }, {
+      headers: {
+        'www-authenticate': await getAuthToken(),
+      },
+    });
+
+    if (response.status === 400) {
+      const apiPayloadError = new Error('API wrong payload');
+
+      apiPayloadError.code = 'api/wrong-payload';
+
+      logger.error(apiPayloadError);
+      throw apiPayloadError;
+    } else if (response.status === 500) {
+      const apiInternalError = new Error('Internal API error');
+
+      apiInternalError.code = 'api/internal-error';
+
+      logger.error(apiInternalError);
+      throw apiInternalError;
+    }
+
+    return response.data;
   } catch (error) {
     logger.error(error);
 
@@ -71,7 +114,7 @@ export async function register(data) {
         formattedError.message = 'Mot de passe trop faible';
         break;
       default:
-        formattedError.message = 'Erreur interne...\nVeuillez réessayer plus tard';
+        formattedError.message = 'Erreur interne...\nVeuillez réessayer ou contacter un administrateur';
         break;
     }
 
