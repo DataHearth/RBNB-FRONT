487ab923152906fc5d95dff5c45f1a85d4c760e3
DataHearth <antoine.l@antoine-langlois.net>
2020-04-08 09:10:16 +0200

    setup firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,39 +1,7 @@
 import * as firebase from 'firebase';
 import firebaseConfig from './firebaseKey';
 
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-//   firebase.analytics();
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {  
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-  });
-  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) { 
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-  });
-  firebase.auth().signOutWithEmailAndPassword(email, password).catch(function(error) { 
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-  });
-  firebase.auth().currentUser.getIdToken(/* forceRefresh */ true).then(function(idToken) {
-    // Send token to your backend via HTTPS
-    // ...
-  }).catch(function(error) {
-    // Handle error
-  });
-
-  try {
-    await firebase.auth().signInWithEmailAndPassword(email, password);
-    const tokenId = await firebase.auth().currentUser.getIdToken();
-    // Store tokenId in session var and/or Cookies
-  } catch (error) {
-    // Error handling
-  }
+export default firebaseApp;
