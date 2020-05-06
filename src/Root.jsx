dc67e0eb697b4cff0dd30b846fb45e4733d76592
DataHearth <antoine.l@antoine-langlois.net>
2020-05-06 16:17:25 +0200

    add jsx extension

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import Home from './Home';
+import Home from './home';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Root extends Component {
   render() {
     return (
-      // eslint-disable-next-line react/jsx-filename-extension
       <BrowserRouter>
         <Route component={Home} path="/" />
       </BrowserRouter>
