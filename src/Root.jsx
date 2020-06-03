8e262b61191124629028c1e864c23da333d9236c
DataHearth <antoine.l@antoine-langlois.net>
2020-06-03 14:53:40 +0200

    Update path

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import Home from './Home2';
+import Home from './Home';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Root extends Component {
