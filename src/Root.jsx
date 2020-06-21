828064fcc795f791b8b0cfa047284045c364da11
DataHearth <antoine.l@antoine-langlois.net>
2020-06-21 18:21:38 +0200

    Update route declaration

--- src/Root.jsx
+++ src/Root.jsx
@@ -5,12 +5,14 @@ import Home from './Components/Home';
 import Singup from './Components/Singup';
 import Search from './Components/Search';
 import Host from './Components/Host';
+import Login from './Components/Login';
 
 export default () => (
   <BrowserRouter>
     <Switch>
       <Route component={Home} exact path="/" />
       <Route component={Singup} exact path="/signup" />
+      <Route component={Login} exact path="/login" />
       <Route component={Search} path="/search/:city" />
       <Route component={Host} path="/host" />
     </Switch>
