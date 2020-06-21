3d5f403ccc6b9e5d0a1bb3186add049409f8903f
DataHearth <antoine.l@antoine-langlois.net>
2020-06-21 18:21:16 +0200

    Add login form and model

---
+++ src/Components/Login.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import {
+  Formik, Form, Field, ErrorMessage,
+} from 'formik';
+import loginSchema from './models/login';
+import { authenticate } from '../lib/authenticate';
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.values = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleSubmit(values, { setSubmitting }) {
+    // eslint-disable-next-line react/prop-types
+    const { history } = this.props;
+    const { email, password } = values;
+
+    authenticate(email, password)
+    // eslint-disable-next-line react/prop-types
+      .then(() => history.push('/'))
+      .catch((error) => {
+        alert(error.message);
+      });
+    setSubmitting(false);
+  }
+
+  render() {
+    return (
+      <div>
+        <Formik
+          initialValues={this.values}
+          onSubmit={this.handleSubmit}
+          validationSchema={loginSchema}
+        >
+          {() => (
+            <Form className="form-horizontal">
+              <label htmlFor="email">
+                Email:
+                <Field type="email" name="email" placeholder="example@email.com" className="form-control" />
+                <ErrorMessage name="email" />
+              </label>
+              <label htmlFor="password">
+                Email:
+                <Field type="password" name="password" className="form-control" />
+                <ErrorMessage name="password" />
+              </label>
+              <button className="btn btn-primary" type="submit">Envoyer</button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Login);
