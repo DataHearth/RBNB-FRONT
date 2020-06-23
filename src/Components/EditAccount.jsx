d70bf7d020be2950e6184c4d6fb8a730ee5c59c6
DataHearth <antoine.l@antoine-langlois.net>
2020-06-23 16:08:07 +0200

    functionnal editAccount

--- src/Components/EditAccount.jsx
+++ src/Components/EditAccount.jsx
@@ -1,44 +1,129 @@
+/* eslint-disable react/destructuring-assignment */
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import axios from 'axios';
-import { Formik, Field, ErrorMessage, Form } from 'formik';
+import {
+  Formik, Field, ErrorMessage, Form,
+} from 'formik';
+import axios from '../lib/axios';
+import firebase from '../lib/firebase';
 import logger from '../lib/logger';
-import userSchema from './models/user';
+import editAccountSchema from './models/editAccount';
 
 class EditAccount extends Component {
   constructor(props) {
     super(props);
-    this.user = {
-      firstname: '',
-      lastname: '',
-      email: '',
-      password: '',
-      address: '',
-      phone: '',
-      picture: '',
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      user: {
+        firstname: '',
+        lastname: '',
+        email: '',
+        password: '',
+        address: '',
+        phone: '',
+        picture: '',
+      },
     };
+    this.dbUser = null;
   }
 
   componentDidMount() {
+    axios.get(`/users/${firebase.auth().currentUser.uid}`).then((res) => {
+      if (res.status === 204) {
+        const apiPayloadError = new Error('Aucun utilisateur, veuillez contacter un administrateur');
+        apiPayloadError.code = 'api/no-user';
+        throw apiPayloadError;
+      } else if (res.status === 500) {
+        const apiInternalError = new Error('Erreur serveur, veuillez contacter un administrateur');
+        apiInternalError.code = 'api/internal-error';
+        throw apiInternalError;
+      }
+
+      this.setState({ user: res.data });
+      this.dbUser = res.data;
+    });
+  }
+
+  handleSubmit(values, { setSubmitting }) {
     const { history } = this.props;
-    axios.get('http://localhost:8080/')
-      .then((res) => {
-        if (res.status === 204) {
-          alert('L\'utilisateur n\'existe pas, veuillez reessayer plus tard...');
+    const userForm = new FormData();
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const [key, value] of Object.entries(values)) {
+      if (key === 'badges') {
+        // TODO: STILL find a solution for this
+        userForm.append('badges', 'no badge');
+        userForm.append('badges', 'no badge');
+      } else {
+        userForm.append(key, value);
+      }
+    }
+    userForm.delete('id');
+
+    // TODO: ugly af, change that made: antoine, to change: antoine
+    if (values.password !== this.dbUser.password) {
+      firebase.auth().currentUser.updatePassword(values.password)
+        .then(() => {
+          axios.post(`/users/${this.dbUser.id}`, userForm).then((res) => {
+            if (res.status === 400) {
+              const apiPayloadError = new Error('Erreur de validation, veuillez contacter un administrateur');
+              apiPayloadError.code = 'api/wrong-payload';
+              throw apiPayloadError;
+            } else if (res.status === 500) {
+              const apiInternalError = new Error('Erreur serveur, veuillez contacter un administrateur');
+              apiInternalError.code = 'api/internal-error';
+              throw apiInternalError;
+            }
+
+            history.push('/');
+          }).catch((error) => {
+            logger.error(error.message, { error });
+            alert(error.message);
+            firebase.auth().signOut();
+            history.push('/');
+          });
+        })
+        .catch((error) => {
+          let errMsg;
+          switch (error.code) {
+            case 'auth/requires-recent-login':
+              errMsg = 'Votre date de connexion est trop ancienne. Veuillez vous reconnecter et reeffectuer l\'operation';
+              break;
+            case 'auth/weak-password':
+              errMsg = 'Votre mot de passe est trop faible...';
+              break;
+            default:
+              errMsg = 'Erreur interne...';
+              break;
+          }
+          logger.error(error.message, { error });
+          alert(errMsg);
           history.push('/');
-        } else if (res.status === 500) {
-          alert('Une erreur interne au serveur est survenue. Veuillez contacter un adminstrateur...');
-          history.push('/');
-        }
-        // eslint-disable-next-line no-restricted-syntax
-        for (const [key, value] of res.data) {
-          this.user[key] = value;
-        }
-      }).catch((error) => {
-        logger.error(error.message, { error });
-        alert('Erreur interne...');
-        history.push('/');
-      });
+        });
+
+      setSubmitting(false);
+      return;
+    }
+
+    axios.post(`/users/${this.dbUser.id}`, userForm).then((res) => {
+      if (res.status === 400) {
+        const apiPayloadError = new Error('Erreur de validation, veuillez contacter un administrateur');
+        apiPayloadError.code = 'api/wrong-payload';
+        throw apiPayloadError;
+      } else if (res.status === 500) {
+        const apiInternalError = new Error('Erreur serveur, veuillez contacter un administrateur');
+        apiInternalError.code = 'api/internal-error';
+        throw apiInternalError;
+      }
+
+      history.push('/');
+    }).catch((error) => {
+      alert(error.message);
+      history.push('/');
+    });
+
+    setSubmitting(false);
   }
 
   render() {
@@ -46,34 +131,14 @@ class EditAccount extends Component {
       <div>
         <h3>Account informations</h3>
         <Formik
-          initialValues={this.user}
+          enableReinitialize
+          initialValues={this.state.user}
           onSubmit={this.handleSubmit}
-          validationSchema={userSchema}
+          validationSchema={editAccountSchema}
         >
           {(props) => (
             <Form>
               <div className="form-row">
-                <div className="col-md-4 mb-3">
-                  <label htmlFor="firstname">
-                    Prénom *
-                    <Field type="text" name="firstname" placeholder="Gérard" className="form-control" />
-                    <ErrorMessage name="firstname" />
-                  </label>
-                </div>
-                <div className="col-md-4 mb-3">
-                  <label htmlFor="lastname">
-                    Nom *
-                    <Field type="text" name="lastname" placeholder="Dupond" className="form-control" />
-                    <ErrorMessage name="lastname" />
-                  </label>
-                </div>
-                <div className="col-md-4 mb-3">
-                  <label htmlFor="email">
-                    Email *
-                    <Field type="email" name="email" placeholder="email@example.com" className="form-control" />
-                    <ErrorMessage name="email" />
-                  </label>
-                </div>
                 <div className="col-md-4 mb-3">
                   <label htmlFor="password">
                     Mot de passe *
